0a889007ac49c2efc375f40e30ab74d53ba7fc73
Khoirul Nasid Furqon <nasidfurqon@gmail.com>
2025-09-22 14:31:50 +0700

    ver: 0.1, feat: add model from database

---
+++ app/Models/Product.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'price', 'stock', 'category_id', 'is_active'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function cartItems()
+    {
+        return $this->hasMany(CartItem::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function wishlistItems()
+    {
+        return $this->hasMany(WishlistItem::class);
+    }
+
+    public function ratings()
+    {
+        return $this->hasMany(ProductRating::class);
+    }
+}
+
