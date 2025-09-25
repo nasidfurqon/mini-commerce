464e277a2f4b7edb433174cd98a098e30978c00a
Khoirul Nasid Furqon <nasidfurqon@gmail.com>
2025-09-26 02:45:25 +0700

    ver: 0.1 feat: menambah routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\{
+    CartController,
+    CartItemController,
+    CategoryController,
+    OrderController,
+    OrderItemController,
+    ProductController,
+    ProductRatingController,
+    UserController,
+    WishlistController,
+    WishlistItemController
+};
+
+Route::resources([
+    'carts'          => CartController::class,
+    'cart-items'     => CartItemController::class,
+    'categories'     => CategoryController::class,
+    'orders'         => OrderController::class,
+    'order-items'    => OrderItemController::class,
+    'products'       => ProductController::class,
+    'product-ratings'=> ProductRatingController::class,
+    'users'          => UserController::class,
+    'wishlists'      => WishlistController::class,
+    'wishlist-items' => WishlistItemController::class,
+]);
